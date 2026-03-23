19fa20d63dc0b9beb0d42967c8bb28737f2acbc0
Evelyn Hacker <hackerevelyn@outlook.com>
2026-03-23 12:12:22 +0100

    wager included plus bug fix diagonal mode

--- Program.cs
+++ Program.cs
@@ -14,180 +14,199 @@ class Program
         const int MODE_ALL_HORIZONTAL = 2;
         const int MODE_ALL_VERTICAL = 3;
         const int MODE_BOTH_DIAGONALS = 4;
+        const int INITIAL_BALANCE = 25;
+        const int WAGER_PER_LINE = 1;
+        const int WIN_PER_LINE = 3;
 
-        Console.WriteLine("Hello, this is a little slot machine game!");
+        int balance = INITIAL_BALANCE;
 
-        //Let user choose mode
         Console.WriteLine(
-            "Please choose a mode (enter number of the mode and press enter):\n1 Middle Line\n2 All Horizontal " +
-            "\n3 All Vertical \n4 Both Diagonals");
-        int mode = Convert.ToInt32(Console.ReadLine());
+            $"Hello, this is a little slot machine game! Wager is $1 per line. You start with {INITIAL_BALANCE} Dollars");
 
-        //Let user choose machine size
-        Console.WriteLine(
-            "Please choose the size of the slot machine (enter number of the size and press enter):\n1 3X3\n2 5X5" +
-            "\n3 7X7");
-        int dim = Convert.ToInt32(Console.ReadLine());
-
-        int size = 0;
-
-        if (dim == DIM_3X3)
+        while (balance > 0)
         {
-            size = SIZE_3X3;
-        }
+            //Let user choose mode
+            Console.WriteLine(
+                "Please choose a mode (enter number of the mode and press enter):\n1 Middle Line\n2 All Horizontal " +
+                "\n3 All Vertical \n4 Both Diagonals");
+            int mode = Convert.ToInt32(Console.ReadLine());
 
-        if (dim == DIM_5X5)
-        {
-            size = SIZE_5X5;
-        }
+            //Let user choose machine size
+            Console.WriteLine(
+                "Please choose the size of the slot machine (enter number of the size and press enter):\n1 3X3\n2 5X5" +
+                "\n3 7X7");
+            int dim = Convert.ToInt32(Console.ReadLine());
 
-        if (dim == DIM_7X7)
-        {
-            size = SIZE_7X7;
-        }
+            int size = 0;
 
-        //Initialize Array
-        int[,] array = new int [size, size];
-
-        //Declare random
-        Random rng = new Random();
-
-        //Populate array
-        for (int row = 0; row < size; row++)
-        {
-            for (int col = 0; col < size; col++)
+            if (dim == DIM_3X3)
             {
-                array[row, col] = rng.Next(0, 3);
-            }
-        }
-
-        //Print array
-        Console.Write("\n");
-        for (int row = 0; row < size; row++)
-        {
-            for (int col = 0; col < size; col++)
-            {
-                Console.Write(array[row, col] + " ");
+                size = SIZE_3X3;
             }
 
-            Console.Write("\n");
-        }
-
-        //Evaluation
-
-        bool bool_win = true;
-        int count_wins = 0;
-
-        if (mode == MODE_MIDDLE_LINE)
-        {
-            int rowToCheck = (size + 1) / 2;
-
-            for (int col = 1; col < size; col++)
+            if (dim == DIM_5X5)
             {
-                if (array[rowToCheck, col] != array[rowToCheck, col - 1])
-                {
-                    bool_win = false;
-                }
+                size = SIZE_5X5;
             }
 
-            if (bool_win == true)
+            if (dim == DIM_7X7)
             {
-                count_wins++;
+                size = SIZE_7X7;
             }
-        }
 
-        if (mode == MODE_ALL_HORIZONTAL)
-        {
+            //While loop over tries
+            //Initialize Array
+            int[,] array = new int [size, size];
+
+            //Declare random
+            Random rng = new Random();
+
+            //Populate array
             for (int row = 0; row < size; row++)
             {
-                bool bool_win_row = true;
+                for (int col = 0; col < size; col++)
+                {
+                    array[row, col] = rng.Next(0, 3);
+                }
+            }
+
+            //Print array
+            Console.Write("\n");
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    Console.Write(array[row, col] + " ");
+                }
+
+                Console.Write("\n");
+            }
+
+            //Evaluation
+
+            bool boolWin = true;
+            int countWins = 0;
+
+            if (mode == MODE_MIDDLE_LINE)
+            {
+                balance -= WAGER_PER_LINE;
+                int rowToCheck = (size + 1) / 2;
+
                 for (int col = 1; col < size; col++)
                 {
-                    if (array[row, col] != array[row, col - 1])
+                    if (array[rowToCheck, col] != array[rowToCheck, col - 1])
                     {
-                        bool_win_row = false;
+                        boolWin = false;
                     }
                 }
 
-                if (bool_win_row == true)
+                if (boolWin)
                 {
-                    count_wins++;
+                    countWins++;
                 }
             }
-        }
 
-        if (mode == MODE_ALL_VERTICAL)
-        {
-            for (int col = 0; col < size; col++)
+            if (mode == MODE_ALL_HORIZONTAL)
             {
-                bool bool_win_col = true;
-                for (int row = 1; row < size; row++)
+                balance -= WAGER_PER_LINE * size;
+                for (int row = 0; row < size; row++)
                 {
-                    if (array[row, col] != array[row - 1, col])
+                    bool boolWinRow = true;
+                    for (int col = 1; col < size; col++)
                     {
-                        bool_win_col = false;
-                    }
-                }
-
-                if (bool_win_col == true)
-                {
-                    count_wins++;
-                }
-            }
-        }
-
-        if (mode == MODE_BOTH_DIAGONALS)
-        {
-            //Diagonal 1
-            bool bool_win_diag = true;
-            for (int col = 1; col < size; col++)
-            {
-                for (int row = 1; row < size; row++)
-                {
-                    if (row == col)
-                    {
-                        if (array[row, col] != array[row - 1, col - 1])
+                        if (array[row, col] != array[row, col - 1])
                         {
-                            bool_win_diag = false;
+                            boolWinRow = false;
                         }
                     }
-                }
 
-                if (bool_win_diag == true)
-                {
-                    count_wins++;
-                }
-            }
-
-            //Diagonal 2
-            bool_win_diag = true;
-            for (int col = size; col < 0; col--)
-            {
-                for (int row = size; row < 0; row--)
-                {
-                    if (row == col)
+                    if (boolWinRow)
                     {
-                        if (array[row, col] != array[row - 1, col - 1])
-                        {
-                            bool_win_diag = false;
-                        }
+                        countWins++;
                     }
                 }
+            }
 
-                if (bool_win_diag == true)
+            if (mode == MODE_ALL_VERTICAL)
+            {
+                balance -= WAGER_PER_LINE * size;
+                for (int col = 0; col < size; col++)
                 {
-                    count_wins++;
+                    bool boolWinCol = true;
+                    for (int row = 1; row < size; row++)
+                    {
+                        if (array[row, col] != array[row - 1, col])
+                        {
+                            boolWinCol = false;
+                        }
+                    }
+
+                    if (boolWinCol)
+                    {
+                        countWins++;
+                    }
                 }
             }
+
+            if (mode == MODE_BOTH_DIAGONALS)
+            {
+                balance -= WAGER_PER_LINE * 2;
+                //Diagonal 1
+                bool boolWinDiag = true;
+                int row = 1;
+                for (int col = 1; col < size; col++)
+                {
+                    if (array[row, col] != array[row - 1, col - 1])
+                    {
+                        boolWinDiag = false;
+                    }
+
+                    row++;
+
+
+                }
+
+                if (boolWinDiag)
+                {
+                    countWins++;
+                }
+
+                //Diagonal 2
+                boolWinDiag = true;
+                row = 1;
+                for (int col = size - 2; col >= 0; col--)
+                {
+                    if (array[row, col] != array[row - 1, col + 1])
+                    {
+                        boolWinDiag = false;
+                    }
+
+                    row++;
+
+                }
+
+                if (boolWinDiag)
+                {
+                    countWins++;
+                }
+            }
+
+            if (countWins > 0)
+            {
+                Console.WriteLine($"Congrats you win {countWins * WIN_PER_LINE} Dollars!");
+                balance += countWins * WIN_PER_LINE;
+            }
+            else
+            {
+                Console.WriteLine($"You loose...");
+                balance -= countWins * WIN_PER_LINE;
+            }
+
+            Console.WriteLine($"Your balance is: {balance}");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
 
-        if (count_wins > 0)
-        {
-            Console.WriteLine("Congrats you win!");
-        }
-        else
-        {
-            Console.WriteLine("You loose...");
-        }
+        Console.WriteLine("No more money left... Game ends!");
     }
 }
